314f09193e20131905f267c09abfef4ecf85a1f3
bvasilop <bvasilop@gmail.com>
2019-03-13 14:15:32 -0700

    added redux-thunk and wired redux to firebase for storing data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,124 +13,117 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref('expenses').on('child_changed', (snapshot) => { // updates subscription changes when child is changed
-	console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-database.ref('expenses').on('child_added', (snapshot) => { // updates subscription changes when child is added. gets called on new and existing children in console
-	console.log(snapshot.key, snapshot.val());
-});
-
-
-
-
-
-
-// database.ref('expenses').on('child_removed', (snapshot) => { // updates subscription changes when child is updated
-// 	console.log(snapshot.key, snapshot.val());
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
 // });
 
-
-
-// database.ref('expenses').on('value', (snapshot) => { // every time expenses changes, we get a brand new array back
-// 		const expenses = [];
-
-// 	snapshot.forEach((childSnapshot) => {
-// 		expenses.push({
-// 			id: childSnapshot.key, // grabs unique id from firebase object
-// 			...childSnapshot.val()
-// 		});
-// 	});
-// 	console.log(expenses);
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
 // });
 
-
-
-// database.ref('expenses') // retrieves data from firebase database
-// .once('value')
-// .then((snapshot) => {
-// 	const expenses = [];
-
-// 	snapshot.forEach((childSnapshot) => {
-// 		expenses.push({
-// 			id: childSnapshot.key, // grabs unique id from firebase object
-// 			...childSnapshot.val()
-// 		});
-// 	});
-// 	console.log(expenses);
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
 // });
+
+// // database.ref('expenses')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const expenses = [];
+
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val()
+// //       });
+// //     });
+
+// //     console.log(expenses);
+// //   });
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //   const expenses = [];
+
+// //   snapshot.forEach((childSnapshot) => {
+// //     expenses.push({
+// //       id: childSnapshot.key,
+// //       ...childSnapshot.val()
+// //     });
+// //   });
+
+// //   console.log(expenses);
+// // });
 
 // database.ref('expenses').push({
-// 	description: 'rent',
-// 	note: 'paid rent',
-// 	amount: 109500,
-// 	createdAt: 98797987149
-// });
-
-
-// database.ref('notes/-L_sEWMfvb-YItvC1PLV').update({
-// 	body: 'Buy Food'
-// });
-
-// database.ref('notes/-L_sEWMfvb-YItvC1PLV').remove();
-
-// database.ref('notes').push({ // creates unique id for list based data
-// 	title: 'Course Topics',
-// 	body: 'React Native, Angular, Python'
-// });
-
-// database.ref().on('value', (snapshot) => {
-// 	const val = snapshot.val();
-// 	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
-// });
-
-// database.ref().set( { // implements data
-// 	name: 'Bill Vasilopoulos',
-// 	age: 43,
-// 	stressLevel: 6,
-// 	job: {
-// 		title: 'Software Developer',
-// 		company: 'Google',
-// 	},
-// 	location: {
-// 		city: 'Chicago',
-// 		country: 'United States'
-// 	},
-// 	instrument: 'Piano'
-// }).then(() => {
-// 	console.log('Data is saved!');
-// }).catch((e) => {
-// 	console.log('This failed', e);
-// });
-
-// database.ref().update({ // updates data
-// 	stressLevel: 8,
-// 	'job/company': 'Amazon',
-// 	'job/city': 'Seattle',
-// 'location/city': 'Seattle'
-// });
-
-// database.ref('isSingle').set(null); // also removed data like remove method
-
-// database.ref('isSingle')
-// .remove()
-// .then(() => {
-// 	console.log('Data was removed');
-// }).catch((e) => {
-// 	console.log('Did not remove data');
-// });
-
-
-// database.ref('location/city') // fetches data from specific props from objects
-// .once('value')
-// .then((snapshot) => {
-// 	const val = snapshot.val();
-// 	console.log(val);
-// })
-// .catch((e) => {
-// 	console.log('Error fetching data', e);
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 976123498763
 // });
 
 
 
 
+
+
+// // database.ref('notes/-Krll52aVDQ3X6dOtmS7').remove();
+
+// // database.ref('notes').push({
+// //   title: 'Course Topics',
+// //   body: 'React Native, Angular, Python'
+// // });
+
+// // database.ref().on('value', (snapshot) => {
+// //   const val = snapshot.val();
+// //   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// // })
+
+// // Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// // Change the data and make sure it reprints
+
+// // database.ref('location/city')
+// //   .once('value')
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((e) => {
+// //     console.log('Error fetching data', e);
+// //   });
+
+// // database.ref().set({
+// //   name: 'Andrew Mead',
+// //   age: 26,
+// //   stressLevel: 6,
+// //   job: {
+// //     title: 'Software developer',
+// //     company: 'Google'
+// //   },
+// //   location: {
+// //     city: 'Philadelphia',
+// //     country: 'United States'
+// //   }
+// // }).then(() => {
+// //   console.log('Data is saved!');
+// // }).catch((e) => {
+// //   console.log('This failed.', e);
+// // });
+
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   'job/company': 'Amazon',
+// //   'location/city': 'Seattle'
+// // });
+
+// // database.ref()
+// //   .remove()
+// //   .then(() => {
+// //     console.log('Data was removed');
+// //   }).catch((e) => {
+// //     console.log('Did not remove data', e);
+// //   });
