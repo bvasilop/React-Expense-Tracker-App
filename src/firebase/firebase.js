eb479bde36dbef4df025374f6124506770bbbb77
bvasilop <bvasilop@gmail.com>
2019-03-18 13:33:12 -0700

    succesfully setup firebase authentication with google

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,20 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
@@ -127,4 +128,3 @@ export { firebase, database as default };
 // //   }).catch((e) => {
 // //     console.log('Did not remove data', e);
 // //   });
-
